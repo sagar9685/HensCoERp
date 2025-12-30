921b3d948c591b2640045c5230cc1eda0e878553
sagar8839 <sagargupta7067@gmail.com>
2025-12-30 12:00:18 +0530

    customer update

--- server/controller/customerController.js
+++ server/controller/customerController.js
@@ -81,3 +81,64 @@ exports.searchCustomersByName = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
+exports.updateCustomer = async(req,res) => {
+  try{
+    const {id} = req.params;
+
+    const {
+      CustomerName,
+      Contact_No,
+      Alternate_Phone,
+      Area,
+      Pincode,
+      Address,
+      GST_No
+    } = req.body;
+
+    if(!id) {
+      return res.status(400).json({message : "Customer id is required"})
+    }
+
+    const pool = await poolPromise;
+    if(!pool) {
+      return res.status(500).json({message : "Db connection failed"})
+    }
+
+    const query = `
+      UPDATE Customers
+      SET
+        CustomerName = @CustomerName,
+        Contact_No = @Contact_No,
+        Alternate_Phone = @Alternate_Phone,
+        Area = @Area,
+        Pincode = @Pincode,
+        Address = @Address,
+        GST_No = @GST_No
+      WHERE CustomerId = @CustomerId
+    `;
+
+    const request = pool.request();
+    request.input("CustomerId", sql.Int, id);
+    request.input("CustomerName", sql.NVarChar,CustomerName);
+     request.input("Contact_No", sql.VarChar, Contact_No);
+    request.input("Alternate_Phone", sql.VarChar, Alternate_Phone || null);
+    request.input("Area", sql.NVarChar, Area);
+    request.input("Pincode", sql.VarChar, Pincode || null);
+    request.input("Address", sql.NVarChar, Address);
+    request.input("GST_No", sql.VarChar, GST_No || null);
+    
+    await request.query(query)
+
+    res.status(200).json({
+      message : "Customer updated successfully"
+    })
+
+  }catch (err) {
+      console.log("Error updating customer:",err)
+      res.status(500).json({
+        message : "Internal server error"
+      })
+  }
+}
--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -5,5 +5,6 @@ const customerController = require('../controller/customerController')
 router.post('/add',customerController.addCustomer);
 router.get('/',customerController.getCustomers);
 router.get('/search', customerController.searchCustomersByName);
+router.put('/update/:id',customerController.updateCustomer)
 
 module.exports=router;
