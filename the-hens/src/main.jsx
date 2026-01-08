c731d44bd6cc1aff9d73a3ed67ee8ecc23b4d557
sagar8839 <sagargupta7067@gmail.com>
2026-01-08 14:56:59 +0530

    user navbar work

--- the-hens/src/main.jsx
+++ the-hens/src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
- 
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
