d4e44c2e492ba1dc09ce40397ad56f94c0aaad8b
sagar8839 <sagargupta7067@gmail.com>
2026-01-08 14:59:56 +0530

    user navbar work

--- the-hens/src/main.jsx
+++ the-hens/src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import App from "./App.jsx";
 import { Provider } from "react-redux";
